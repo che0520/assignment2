ecfaee30bc468d02b7d5904919e687f5c62bb858
che0520 <che0520@mail.green1450.com>
2024-10-02 08:46:35 +0800

    test1

---
+++ src/main/java/ntut/posd2024f/shapes/App.java
@@ -0,0 +1,10 @@
+package ntut.posd2024f.shapes;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/ntut/posd2024f/shapes/AppTest.java
@@ -0,0 +1,19 @@
+package ntut.posd2024f.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
