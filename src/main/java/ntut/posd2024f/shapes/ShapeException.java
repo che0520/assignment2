02dbe0d78452f6ccb8ef2d3affc5ef28866690d2
Bryan Tsai <twbikeman@gmail.com>
2024-10-05 23:55:52 +0800

    finished

--- src/main/java/ntut/posd2024f/shapes/ShapeException.java
+++ src/main/java/ntut/posd2024f/shapes/ShapeException.java
@@ -1,9 +1,12 @@
 package ntut.posd2024f.shapes;
 
 public class ShapeException extends RuntimeException{
-    
+    private String s;
     public ShapeException(String message) {
-        super(message);
+        s = message;
+
     }
-   
+    public String toString() {
+        return s;
+    }
 }
