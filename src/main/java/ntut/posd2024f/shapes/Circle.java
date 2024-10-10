7343eaf53f2306877c9ab4627fc534b8e0cab14e
Your Name <you@example.com>
2024-10-10 14:19:17 +0800

    finished circle

--- src/main/java/ntut/posd2024f/shapes/Circle.java
+++ src/main/java/ntut/posd2024f/shapes/Circle.java
@@ -1,39 +1,48 @@
 package ntut.posd2024f.shapes;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Circle implements Shape {
     private double radian;
    
 
-    public Circle(Double... args) throws ShapeException{
-        if (args.length != 1)
+    public Circle(double args) throws ShapeException{
+        if (args < 0)
             throw new ShapeException("It's not a circle!");
-        radian = args[0];
-
+        radian = args;
     }
 
+
+
+
+    public Circle(ArrayList<Double> args) throws ShapeException{
+        if (args.size() !=  1)
+            throw new ShapeException("It's not a circle!");
+        radian = args.get(0);
+
+        
+
+    }
+    
+    @Override
     public double area() {
         return radian * radian * 3.14;
     }
 
+    @Override
     public double perimeter() {
         return 2 * radian * 3.14;
     }
     
     @Override
     public void add(Shape s) throws ShapeException {
-        throw new ShapeException("Illegal Operaton");
+        throw new ShapeException("Illegal Operation");
     }
     
     @Override
-    public NullIterator iterator() {
-        return new ShapeIterator();
+    public Iterator<Shape> iterator() {
+        return new NullIterator();
     }
-
-    private class ShapeIterator implements NullIterator {
-        public boolean hasNext() {return false;}
-        public Shape next() {return null;}; 
-    }
-
-
 
 }
--- src/test/java/ntut/posd2024f/shapes/CircleTest.java
+++ src/test/java/ntut/posd2024f/shapes/CircleTest.java
@@ -1,30 +1,84 @@
 package ntut.posd2024f.shapes;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+
 
 public class CircleTest {
     @Test
-    void CircleCreate() 
+    void CircleCreateException1() 
     {
-        ShapeException excep = assertThrows(ShapeException.class, ()->{new Circle(1.0, 1.0);});
-        assertEquals("ntut.posd2024f.shapes.ShapeException: " + "It's not a circle!", excep.toString());
-        
-      
-     
+        double b = -1;
+        // Circle c = new Circle(arr);
+        ShapeException excep = assertThrows(ShapeException.class, () -> {new Circle(b);});
+        assertEquals("ntut.posd2024f.shapes.ShapeException: " + "It's not a circle!", excep.toString());      
     }
+
+
     @Test
-    void CirclePerimeterArea() {
+    void CircleCreateException2() 
+    {
+        ArrayList<Double> arr = new ArrayList<>();
+        arr.add(1.0);
+        arr.add(1.0);
+        // Circle c = new Circle(arr);
+        ShapeException excep = assertThrows(ShapeException.class, () -> {new Circle(arr);});
+        assertEquals("ntut.posd2024f.shapes.ShapeException: It's not a circle!", excep.toString());      
+    }
+
+
+    @Test
+    void CirclePerimeter() {
 
         try {
             Circle c = new Circle(1.0);
             assertEquals(6.28, c.perimeter());
-            assertEquals(3.14, c.area());
             } catch (ShapeException e) {
                 System.out.println(e);
             }
 
 
+    }
+
+    @Test
+    void CircleArea() {
+
+        try {
+            Circle c = new Circle(1.0);
+            assertEquals(3.14, c.area());
+            } catch (ShapeException e) {
+                System.out.println(e);
+            }
+    }
+
+
+    @Test
+    void CircleAdd() {
+
+        try {
+            Circle c1 = new Circle(1.0);
+            Circle c2 = new Circle(1.0);
+            ShapeException excep = assertThrows(ShapeException.class, () -> {c1.add(c2);});
+            assertEquals("ntut.posd2024f.shapes.ShapeException: Illegal Operation", excep.toString());
+            } catch (ShapeException e) {
+                System.out.println(e);
+            }
+    }
+
+    @Test
+    void CircleIterator() {
+
+        try {
+            Circle c1 = new Circle(1.0);
+            Iterator<Shape> it = c1.iterator();
+            assertTrue(it instanceof NullIterator);
+            } catch (ShapeException e) {
+                System.out.println(e);
+            }
     }
 }
